1fa6669a11f3a5952b192ab1dcc88b3a53b2f640
Flavio Miranda <flaviomiranda@alunos.utfpr.edu.br>
2024-10-20 15:46:09 -0300

    Trabalhei parte da Aula01
    Produzir uma parte da Aula01 criando duas pastas, faltando o restante da aula.

---
+++ Aula01.Console.Estruturado.Level.Statement/Program.cs
@@ -0,0 +1,21 @@
+﻿const int MaximoDeCompromissos = 100;
+
+string[] datas = new string[MaximoDeCompromissos];
+// var horas = new string[MaximoDeCompromissos];
+
+System.Console.WriteLine(" 1. Adicionar Compromisso");
+
+var opcao = Console.Readline();
+
+if (opcao=="1"){
+    System.Console.Write;("Data: ");
+    var dataInformada = System.Console.ReadLine();
+    if (dataInformada != ""){
+        datas[0] = dataInformada;
+        System.Console.WriteLine("Data " + dataInformada + " registrada com sucesso");  
+    } else {
+             System.Console.WriteLine("Data inválida");
+             
+    }
+    
+}
